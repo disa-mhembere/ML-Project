16579b52244567b385f61d1b641220852d91a16c
Disa Mhembere <dmhembe1@jhu.edu>
2014-03-30 23:11:21 -0400

    [Data]: added email parser

---
+++ src/email_parser.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+# email_parser.py
+# Created by Disa Mhembere on 2014-03-30.
+# Email: disa@jhu.edu
+# Copyright (c) 2014. All rights reserved.
+"""
+Example:
+['Message-ID',
+'Date',
+'From',
+'To',
+'Subject',
+'Cc',
+'Mime-Version',
+'Content-Type',
+'Content-Transfer-Encoding',
+'Bcc',
+'X-From',
+'X-To',
+'X-cc',
+'X-bcc',
+'X-Folder',
+'X-Origin',
+'X-FileName']
+"""
+def get_date():
+  """ Extract and return """
+  #data = msg.get("Date")
+  pass
+
+def during_business():
+  """ Return bool"""
+  pass
+
+def get_from():
+  pass
+
+def get_to():
+  pass
+
+def get_cc():
+  pass
+
+def get_bcc():
+  pass
+
+def get_email_length():
+  pass
+
+def get_in_
