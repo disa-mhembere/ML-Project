587af4a91a7f4a2388bbb7ab9882f488115761c1
Kunal Lillaney <klillan1@jhu.edu>
2014-04-02 23:09:40 -0400

    Adding email_parser.py

--- src/utils/email_parser.py
+++ src/utils/email_parser.py
@@ -24,28 +24,63 @@ Example:
 'X-Origin',
 'X-FileName']
 """
-def get_date():
-  """ Extract and return """
-  #data = msg.get("Date")
-  pass
+import email
+import pdb
+
+def get_date( msg ):
+  """ Extracts date from the email and returns a String """
+  return msg.get("Date")
 
 def during_business():
   """ Return bool"""
   pass
 
-def get_from():
-  pass
+def get_from( msg ):
+  """ Extracts the "from" from the email as a String """
+  return msg.get('From')
 
-def get_to():
-  pass
+# I am yet to strip the string here of /r/n/t
+def get_to( msg ):
+  """ Extracts the "to" from the email as a String """
+  return msg.get('To')
 
-def get_cc():
-  pass
+# Same as above
+def get_cc( msg ):
+  """ Extracts the "cc" from the email as List of Strings """
+  return msg.get('Cc')
 
-def get_bcc():
-  pass
+# Same problem
+def get_bcc( msg ):
+  """ Extracts the "bcc" from the email as a List of Strings """
+  return msg.get('Bcc')
 
-def get_email_length():
-  pass
+# This does not return the length of the attachment becuase our data does not have an attachment
+def get_email_length( msg ):
+  """ Extracts the email payload and returns the length of the payload """
+  return len(msg.get_payload())
 
-def get_in_
+# I assume that this is to check the email contains an attachement or not
+# But this does not work and I am trying to think of another way to figure that there is an attachment.
+def get_in_( msg ):
+  """ Retuns a true if there is an attachement and a false if there is none """
+  return ( msg.has_key('X-FileName') )
+
+# I think this becomes defunct given the disucssion we had.
+def get_entry( msg ):
+  """ Returns an entry of type <something> """
+  
+  pdb.set_trace()
+  print get_from(msg)
+
+
+def main():
+  """ Test Function for this file. Grabs a random file and passes the contents to get_entry for test """
+  
+  # I am testing for two diffrent files here. Once which contains an attachment and the other does not.
+  test1 = open("/home/klillan1/work/machine_learning/data/enron_mail_20110402/maildir/kean-s/transco/1.",'r')
+  test2 = open("/home/klillan1/work/machine_learning/data/enron_mail_20110402/maildir/kitchen-l/inbox/50.", 'r')
+  msg = email.message_from_file(test1)
+  get_entry( msg )
+
+if __name__ == "__main__":
+  main()
