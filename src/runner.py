cc8da8adfd9e90608f8af6f36f3e2b867d8eef33
Disa Mhembere <dmhembe1@jhu.edu>
2014-04-07 02:13:30 -0400

    [Core]: Fully functional runner.py. More testing needed

--- src/runner.py
+++ src/runner.py
@@ -9,18 +9,16 @@
 """
 Key:
   - _in : in network
-  - _out : outn of network
+  - _outn : out of network
   - _dbh : during business hours
   - _abh : aftere business hours
 
 Feature vector order:
   [to_email_in,  from_email_in, cc_to_email_in, bcc_to_email_in, mean_email_len_in, 
-  to_email_outn,  from_email_out, cc_to_email_out, bcc_to_email_out, mean_email_len_out,
+  to_email_outn,  from_email_outn, cc_to_email_outn, bcc_to_email_outn, mean_email_len_outn,
   num_attachments, on_weekend, on_weekday, num_emails_dbh, num_emails_abh, degree, 
   scan1, triangle, transitivity, normed_eigenvalue
   ]
-
-
 """
 import argparse
 import os
@@ -28,50 +26,65 @@ from email_parser import *
 import email
 from structs.time_series_fv import tsfv
 from utils.names_to_ids import People
+from glob import glob
 
-def run(data_dir, names_fn):
+def run(data_dir, names_fn, save_fn):
   """ 
   Given a directory of emails lets get all features and build a 
   data structures with these features and graph statistics.
 
   @param data_dir: is the directory with all email mailboxes
   @param names_fn: the file containing the names to email address mapping
+  @param save_fn: the file name to save pickle dump after
   """
-  names_dict = People(names_fn)
-  WEEKS = 189
+  people = People(names_fn)
   NUM_FEATURES = 20
 
-  tsfv_obj = tsfv(NUM_FEATURES, WEEKS)
+  tsfv_obj = tsfv(NUM_FEATURES)
 
-  for people_dir in os.listdir(data_dir): # Names of employees
-    for email_cat in os.listdir(people_dir): # e.g Cat = category sent, inbox
-      for email_fn in os.listdir(email_cat):
+  for people_dir in glob(os.path.join(data_dir,"*")): # Names of employees
+    print "Processing employee ' %s' ..." % people_dir
+    for email_cat in glob(os.path.join(people_dir, "*")): # e.g Cat = category sent, inbox
+      for email_fn in glob(os.path.join(email_cat, "*")):
+        print "  Processing file %s/%s" % (email_cat, email_fn)
 
         entry = email.message_from_file(open(email_fn))
+
         # This is a single email
-        _week = get_week(entry)
-        assert _week > 0 and _week <= WEEKS, "Week specified out of range of WEEKS = %d" % WEEKS
-        _to = get_to(entry)
-        _from =
+        week = get_week(entry) # 2-tuple
+        to, to_email_in, to_email_outn = get_to(entry) # list of people whom this is sent to 
+        cc, cc_to_email_in, cc_to_email_outn = get_cc(entry) # list of people who are cc'd
+        bcc, bcc_to_email_in, bcc_to_email_outn = get_bcc(entry) # bcc'd
+        _id = people.get_id(get_from(entry))
+        weekday = on_weekday(entry)
+        
+        # Update for a single mail
+        tsfv_obj.insert_email(week, _id, to_email_in, to_email_outn, cc_to_email_in, 
+            cc_to_email_outn, bcc_to_email_in, bcc_to_email_outn, during_business(entry), 
+            weekday, get_email_length(entry), has_attachment(entry))
+        
+        # Update cc-ers
+        for recepient in to:
+          tsfv_obj.update_cc(week, recepient_id=people.get_id(recepient), sender_id=_id)
 
-        fv = [get()] # featurevectors
-
-
-
-
-
-
-
-
+        for recepient in cc:
+          tsfv_obj.update_bcc(week, recepient_id=people.get_id(recepient), sender_id=_id)
+        
+  print "Mission complete!"
+  tsfv_obj.save(save_fn)
 
 def main():
   parser = argparse.ArgumentParser(description="Extract data from emails and write \
       to file in data format")
   parser.add_argument("data_dir", action="store", help="The dir containing data")
-  parser.add_argument("data_dir", action="store", help="The dir containing data")
-  parser.add_argument("-O", "--OPT", action="", help="")
-  result = parser.parse_args()
+  parser.add_argument("names_fn", action="store", help="The file with mapping from email \
+      to name data")
+  parser.add_argument("-s", "--save_fn", action="store", default="tsfv.cPickle", \
+      help="The tsfv file name of the pickle dump")
 
+  result = parser.parse_args()
+  
+  run(result.data_dir, result.names_fn, result.save_fn)
 
 if __name__ == "__main__":
   main()
