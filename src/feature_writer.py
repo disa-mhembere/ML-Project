7f39cbbae1496d014958eb7001cd194ef3864455
Kunal Lillaney <klillan1@jhu.edu>
2014-04-23 21:15:58 -0400

    [DS] Adding the feature writer to write tsfv

--- src/feature_writer.py
+++ src/feature_writer.py
@@ -4,3 +4,24 @@
 # Created by on 2014-03-30.
 # Email: disa@jhu.edu | lillaney@jhu.edu
 # Copyright (c) 2014. All rights reserved.
+
+import operator
+import pdb
+
+def write_file( tsfv ):
+  """ Writes a TSFV to a file. """
+  filename = open('test','wb')
+
+  for i in sorted( tsfv.iteritems(), key=operator.itemgetter(0) ):
+    for j in i[1].iteritems():
+      for k in j[1]:
+        filename.write( str(k)+'\n' )
+
+def main():
+
+  x = {1: [1,2], 3: [1,4], 4:[1,3], 2:[1,10], 0:[0,9]}
+  y = {4:x, 1:x, 2:x}
+  write_file(y)
+
+if __name__ == "__main__":
+  main()
