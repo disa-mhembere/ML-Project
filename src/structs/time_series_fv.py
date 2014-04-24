88d50fea63d0479a6e7d4ce5c70b5785145b791d
Disa Mhembere <dmhembe1@jhu.edu>
2014-04-24 04:08:22 -0400

    [DS]: Adding capability to update myself

--- src/structs/time_series_fv.py
+++ src/structs/time_series_fv.py
@@ -11,12 +11,24 @@ from os import remove
 
 """
 Feature vector order:
-  [to_email_in,  from_email_in, cc_to_email_in, bcc_to_email_in, mean_email_len_in,
-  to_email_outn,  from_email_out, cc_to_email_out, bcc_to_email_out, mean_email_len_out,
+  [to_email_in,  from_email_in, cc_to_email_in, bcc_to_email_in, mean_email_len,
+  to_email_outn,  from_email_outn, cc_to_email_outn, bcc_to_email_outn,
   num_attachments, on_weekend, on_weekday, num_emails_dbh, num_emails_abh, degree,
   scan1, triangle, transitivity, normed_eigenvalue
   ]
 """
+CC_INDEX_IN = 2
+CC_INDEX_OUTN = 7
+
+BCC_INDEX_IN = 3
+BCC_INDEX_OUTN = 8
+
+TO_INDEX_IN = 0
+TO_INDEX_OUTN = 5
+
+EMAIL_LEN_INDEX = 4
+INV_INDEX_START = 15
+
 class tsfv(object):
   
   def __init__(self, num_features):
@@ -42,30 +54,71 @@ class tsfv(object):
       self.data[week] = {}
 
     if not self.data[week].has_key(_id):
-      self.data[week][_id] = np.array([0]*self.num_features)
+      self.data[week][_id] = np.array([0]*self.num_features, dtype=np.float32) # NOTE: float32
     
     self.data[week][_id] += count_list
 
-  def update_to(email_add, week):
-    """ for a single email update a users to count """
-    pass # TODO
+  def verify(self, week, _id):
+    assert isinstance(_id, int), "ID must be an int"
+    if not self.data.has_key(week):
+      self.data[week] = {}
+
+    if not self.data[week].has_key(_id):
+      self.data[week][_id] = np.array([0]*self.num_features, dtype=np.float32) # NOTE: float32
 
   def insert_email(self, week, _id, to_email_in, to_email_outn, cc_to_email_in,
      cc_to_email_outn, bcc_to_email_in, bcc_to_email_outn, during_business,
      weekday, email_length, attachment):
-    # weekend = int(not weekday),
-    # outside_business = int(not during_business)
-    pass # TODO
 
-  def update_cc(self, week, recepient_id, sender_id):
-    pass # TODO
+    self.verify(week, _id) # Wasteful!
+    self.data[week][_id] += [ to_email_in, 0, cc_to_email_in, bcc_to_email_in, email_length,
+        to_email_outn, 0, cc_to_email_outn, bcc_to_email_outn, attachment,
+        int(not weekday), weekday, during_business, int(not during_business), 0, 0, 0, 0, 0 
+        ]
 
-  def update_bcc(self, week, recepient_id, sender_id):
-    pass # TODO
+  def update_to(self, week, recepient_id, is_in_network):
+    self.verify(week, recepient_id) # Wasteful!
 
+    if is_in_network:
+      self.data[week][recepient_id][TO_INDEX_IN] += 1
+    else: 
+      self.data[week][recepient_id][TO_INDEX_OUTN] += 1
+
+
+  def update_cc(self, week, recepient_id, is_in_network):
+    self.verify(week, recepient_id) # Wasteful!
+
+    if is_in_network:
+      self.data[week][recepient_id][CC_INDEX_IN] += 1
+    else:
+      self.data[week][recepient_id][CC_INDEX_OUTN] += 1
+
+
+  def update_bcc(self, week, recepient_id, is_in_network):
+    self.verify(week, recepient_id) # Wasteful!
+    if is_in_network:
+      self.data[week][recepient_id][BCC_INDEX_IN] += 1
+    else:
+      self.data[week][recepient_id][BCC_INDEX_OUTN] += 1
+
+  def _update_inv(self, week, _id, invariants):
+    """
+    Add the precomputed invariants 
+    """
+    self.verify(week, _id) # Wasteful!
+
+    self.data[week][_id][INV_INDEX_START:] = invariants
+
+  def finish(self, ):
+    for week in self.data.keys():
+      for _id in self.data[week].keys():
+        sum_emails = self.data[week][_id][TO_INDEX_IN] +\
+            self.data[week][_id][TO_INDEX_OUTN] 
+        if sum_emails: # Divisor cannot be zero but OK if numerator is 
+          self.data[week][_id][EMAIL_LEN_INDEX] /= float(sum_emails)
 
   def __repr__(self):
-    """ Print me"""
+    """ Print me """
     s = ""
     for week in sorted(self.data.keys()):
       for person_id in sorted(self.data[week].keys()):
