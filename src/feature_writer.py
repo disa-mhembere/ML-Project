c566f9338bb48c8ff00413bcb8926d3e227645c1
Kunal Lillaney <klillan1@jhu.edu>
2014-05-02 18:34:57 -0400

    [BUG] Fixing the read and write formats. Using Arguments for input.

--- src/feature_writer.py
+++ src/feature_writer.py
@@ -6,25 +6,40 @@
 # Copyright (c) 2014. All rights reserved.
 
 import operator
+import argparse
 import cPickle
 import pdb
 
-def write_file( tsfvName ):
+def write_file( tsfvName, writeFile ):
   """ Writes a TSFV to a file. """
+  
   fs = open(tsfvName,'r')
   tsfv = cPickle.load(fs)
-  filename = open('test','wb')
+  filename = open(writeFile, 'wb')
 
-  for i in sorted( tsfv.iteritems(), key=operator.itemgetter(0) ):
+  for i in sorted( tsfv.data.iteritems(), key=operator.itemgetter(0) ):
+    
     for j in i[1].iteritems():
-      for k in j[1]:
-        filename.write( str(k)+'\n' )
+      
+      for idx, value in enumerate(j[1]):
+       
+        filename.write( "{}:{} ".format( idx+1, str(value) ) )
+
+      filename.write('\n')
 
 def main():
 
   x = {1: [1,2], 3: [1,4], 4:[1,3], 2:[1,10], 0:[0,9]}
   y = {4:x, 1:x, 2:x}
-  write_file(y)
+  #write_file(y)
+
+  parser = argparse.ArgumentParser(description="Write the tsfv to file")
+  parser.add_argument("read_file", action="store", help="File Name to read from")
+  parser.add_argument("write_file", action="store", help="File Name to write to")
+
+  result = parser.parse_args()
+
+  write_file(result.read_file, result.write_file)
 
 if __name__ == "__main__":
   main()
