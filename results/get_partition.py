28b72becbd5b9138f44b57c69ddb57ac629ec8e6
Disa Mhembere <dmhembe1@jhu.edu>
2014-05-13 00:52:38 -0400

    [Wu]: Added excel sheets

--- results/get_partition.py
+++ results/get_partition.py
@@ -15,10 +15,11 @@ def write_subsection(fn, n):
 
     if count == n: break
 
-  out_fn = "enron_plus_%d_composite.cPickle" % (n-200)
+  out_fn = "enron_inv_plus_%d_composite.cPickle" % (n-200) # Hack!
+  print "Writing %s" % out_fn
   cPickle.dump(d, open(out_fn ,"wb"))
 
 if len(sys.argv) < 2:
-  print "usage: get_partition.py pickle filename, number_of_id"
+  print "usage: get_partition.py pickle_filename number_of_id"
   exit(-1)
 write_subsection(sys.argv[1], int(sys.argv[2])) 
